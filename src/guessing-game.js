1c07b67e9f56f47e3cae97fc9416ab1a3378e4b0
Abzal <Abzal.Tankibayev@outlook.com>
2019-10-07 11:08:28 +0600

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
+// Игра угадать число
+// Стратегия 50/50
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min = 0;
+        this.max = 1;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return this.average();
     }
-
     lower() {
-
+        this.max = this.average();
     }
-
     greater() {
-
+        this.min = this.average();
+    }
+    average() {
+        return Math.ceil((this.max+this.min)/2);
     }
 }
 
